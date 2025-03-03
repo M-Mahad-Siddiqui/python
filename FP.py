93c84bc36c4e3d07f32fcaacaf42bae0d400ebb4
M-Mahad-Siddiqui <144838103+M-Mahad-Siddiqui@users.noreply.github.com>
2025-03-03 17:58:04 +0500

    add fp.py

---
+++ FP.py
@@ -0,0 +1,74 @@
+# name = "tauliah"
+# print("hello " , name)
+# naam = input("enter your name : ")
+# print("hello " , naam)
+# age = input("enter your age : ")
+# age = int(age) + 2
+# print("your age is " , age)
+
+# first = input("enter your first  : ")
+# last = input("enter your last  : ")
+
+# sum= int(first) + int(last)
+# print("your sum is " , sum)
+
+# name = "tauliah siddiqui"
+# print(name.upper()) 
+# print(name.find("s"))
+# print(name.replace("t" , "d"))
+# print("tauliah" in name)
+# num = 45
+# b = 45%10
+# a = 45//10
+# print(b,a)
+
+# if(a%2!=0):
+#     print("even")
+# else:
+#     print("odd")
+
+# num1 = int(input("enter your first number : "))
+# num2 = int(input("enter your second number : "))
+# operator = input("enter your operator : ")
+
+# if(operator == "+"):
+#     print(num1 + num2)
+# elif(operator == "-"):
+#     print(num1 - num2)
+# elif(operator == "*"):
+#     print(num1 * num2)
+# elif(operator == "/"):
+#     print(num1 / num2)
+# else:
+#     print("invalid operator")
+
+# number = range(10)
+# for i in number:
+#     print(i)
+
+number = 1
+while number <= 5:
+    print(number * "*")
+    number += 1
+
+marks = [80,70,60,50,78]
+marks.insert(0,90)
+print(marks[0:2])
+print(len(marks))
+
+result = {
+    "tauliah" : 80,
+    "aliza" : 70,
+    "fasih" : 60,
+    "tahir" : 50,
+    "fahad" : 78,
+}
+result["umer"] = 49
+print(result["tauliah"])
+print(result.keys())
+print(result.values())
+
+def multiplication(num1 , num2):
+    print(num1 * num2)
+multiplication(2,3)
+
